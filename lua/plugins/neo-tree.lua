88de43e24f750acab2f1b81d78a6b679fcf255f0
quynezz <laungocquyen098@gmail.com>
2025-04-30 01:33:20 +0700

    match the color theme and erase the background number line

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,41 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    event_handlers = {
+      {
+        event = "neo_tree_buffer_enter",
+        handler = function()
+          -- Enable relative line numbers
+          vim.opt_local.relativenumber = true
+          -- Enable cursorline to highlight the current line number
+          vim.opt_local.cursorline = true
+          vim.opt_local.cursorlineopt = "number"
+          -- Set LineNr to pale white with no background
+          vim.api.nvim_set_hl(0, "LineNr", { fg = "#C1C0C0", bg = nil })
+          -- Set CursorLineNr to the same pale white with no background
+          vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#fbcb97", bg = nil, bold = true })
+          -- Ensure CursorLine has no background (affects the entire line, not just the number)
+          vim.api.nvim_set_hl(0, "CursorLine", { bg = nil })
+        end,
+      },
+      {
+        event = "neo_tree_buffer_leave",
+        handler = function()
+          -- Disable cursorline when leaving the Neo-tree buffer
+          vim.opt_local.cursorline = false
+        end,
+      },
+    },
+  },
+  init = function()
+    -- Ensure the highlight persists after colorscheme changes
+    vim.api.nvim_create_autocmd("ColorScheme", {
+      pattern = "*",
+      callback = function()
+        vim.api.nvim_set_hl(0, "LineNr", { fg = "#C1C0C0", bg = nil })
+        vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#fbcb97", bg = nil, bold = true })
+        vim.api.nvim_set_hl(0, "CursorLine", { bg = nil })
+      end,
+    })
+  end,
+}
