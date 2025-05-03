609912931c54c2ddb15b21e011fd35979924b83f
quynezz <laungocquyen098@gmail.com>
2025-05-03 23:44:27 +0700

    remove trailing space and format code

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,17 +12,24 @@ keymap.set("n", "<C-l>", "<C-w>l", opts)
 keymap.set("n", "<leader>sv", ":vsplit<CR>", opts)
 keymap.set("n", "<leader>sh", ":split<CR>", opts)
 keymap.set("n", "<leader>m", ":MaximizeToggle<CR>", opts)
-
---Identing
+-- Indenting
 keymap.set("v", "<", "<gv")
 keymap.set("v", ">", ">gv")
 
---Comment
+-- Comment
 vim.api.nvim_set_keymap("n", "<C-_>", "gcc", { noremap = false })
 vim.api.nvim_set_keymap("v", "<C-_>", "gcc", { noremap = false })
 
 -- Define :FormatCode command
 vim.api.nvim_create_user_command("FormatCode", function()
   vim.cmd("normal! gggqG``") -- Execute gggqG`` in normal mode
-  vim.notify("      Done Formatting!    ")
+  vim.notify("Done Formatting!")
 end, { desc = "Format entire file and return cursor to previous position" })
+
+-- Remove trailing whitespace on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    vim.cmd([[%s/\s\+$//e]])
+  end,
+})
