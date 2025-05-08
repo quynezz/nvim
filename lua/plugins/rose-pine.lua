5fd4c25386334744e62f3d27527db1ca48fd1506
quynezz <laungocquyen098@gmail.com>
2025-05-08 22:47:58 +0700

    good for now

--- lua/plugins/rose-pine.lua
+++ lua/plugins/rose-pine.lua
@@ -6,7 +6,7 @@ return {
     config = function()
       require("rose-pine").setup({
         variant = "auto", -- auto, main, moon, or dawn
-        dark_variant = "main", -- main, moon, or dawn
+        dark_variant = "moon", -- main, moon, or dawn
         dim_inactive_windows = false,
         extend_background_behind_borders = true,
 
