6db02ee2cb3e30c7848c55299e82fef98fbdd415
quynezz <laungocquyen098@gmail.com>
2025-05-27 16:58:13 +0700

    removing autoformatitng globally

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,18 +1,18 @@
--- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Options are automatically loaded before lazy.nvim startup
 -- Add any additional options here
 --vim.opt.clipboard = "unnamedplus"
---
---
 -- In ~/.config/nvim/lua/config/options.lua or a similar file
---
 -- Removing the trailing space "--"
 vim.opt.list = false
+-- Disable all the animations
 vim.g.snacks_animate = false
+-- Disable the autoformatting
+vim.g.autoformat = false
+
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
 vim.opt.expandtab = true
-vim.opt.autoindent = true
-vim.opt.smartindent = true
 -- Change all the cursor line (visual, insert and normal) into "block"
 vim.opt.guicursor = "n-v-c-i-ci-ve-r-cr-o:block"
